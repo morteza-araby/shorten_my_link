9541c946fb468bd9348711d8a125c2145f65fca7
Morteza Araby <morteza.araby@ericsson.com>
2017-06-13 14:27:20 +0200

    link create validation added. Add meteor method insert,which will be run both in server and client

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
 export default class LinkCreate extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: '',
+		};
+	}
+	handleSubmit(e){
+		e.preventDefault();
+
+		// pull the value out and validate it
+		Meteor.call('links.insert', this.refs.input.value, (error) => {
+			if(error){
+				this.setState({ error: error.message});
+			} else {
+				this.setState({ error: ''});
+				this.refs.input.value = '';
+			}
+		});
+
+	}
 	render() {
 		return (
-			<form>
+			<form onSubmit={this.handleSubmit.bind(this)}>
 				<div className="form-group">
 					<label>Link to be shorten</label>
-					<input className="form-control" />					
+					<input ref="input" className="form-control" />					
 				</div>
+				<div className="text-danger">{this.state.error}</div>
 				<button className="btn btn-primary">Shorten</button>
 			</form>
 		);
